a8ae417338ef491a1ec9eec2e03d32775b92e20e
Andresmab <amarizab@correo.udistrital.edu.co>
2023-04-19 15:58:19 -0500

    Update Modelo Transporte con ejemplo.py

--- Modelo Transporte/Modelo Transporte con ejemplo.py
+++ Modelo Transporte/Modelo Transporte con ejemplo.py
@@ -4,18 +4,26 @@ from pulp import *
 from pandas import DataFrame
 
 
-### Ciudades
-origen = ['LA','Detroit','New Orleans']
-destino = ['Denver','Miami']
+##Una empresa de transporte p´ublico llamada Transunion tiene 3 rutas diferentes R1, R2, R3
+##a lo largo de la ciudad de Bogot´a. Cada una de las rutas tiene la posibilidad de subir 40,
+##35, 35 pasajeros respectivamente , las cuales deben ser usadas para 4 diferentes estaciones
+##donde esperan 50, 20, 30, 10 pasajeros respectivamente en cada estaci´on. Halle el costo de
+##transporte minimo, usando los 3 diferentes m´etodos del modelo de transporte.
 
-oferta = {'LA': 1000, 'Detroit' : 1500, 'New Orleans': 1200}
-demanda = {'Denver': 2300, 'Miami' : 1400}
 
-costo_envio ={'LA':{'Denver': 80, 'Miami' : 215},
-             'Detroit':{'Denver': 100, 'Miami' : 108},
-             'New Orleans': {'Denver': 102, 'Miami' : 68}}
+### Origenes R1, R2,R3 Que son las rutas de los buses y destinos E1, E2, E3, E4 que son las estaciones
 
-### Declaramos la función objetivo... nota que buscamos minimizar el costo(LpMinimize)
+origen = ['R1','R2','R3']
+destino = ['E1','E2', 'E3', 'E4']
+
+oferta = {'R1': 40, 'R2' : 35, 'R3': 35}
+demanda = {'E1': 50, 'E2' : 20, 'E3' : 30, 'E4' :10}
+
+costo_envio ={'R1':{'E1': 2, 'E2' : 4, 'E3' : 3, 'E4' :3},
+             'R2':{'E1': 3, 'E2' : 2, 'E3' : 6, 'E4' :2},
+             'R3': {'E1': 3, 'E2' : 1, 'E3' : 4, 'E4' :5}}
+
+### Declaramos la  función objetivo... nota que buscamos minimizar el costo(LpMinimize)
 prob = LpProblem('Transporte', LpMinimize)
 
 rutas = [(i,j) for i in origen for j in destino]
